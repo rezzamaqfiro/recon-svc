831762fe3f3432b2936d6a4431b0cfe977d4ec52
Maqfiro <rezzamaqfiro@gmail.com>
2026-01-14 00:54:27 +0700

    update recon func parameter call

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	// Define flag for CLI
 	sysPath := flag.String("system", "", "Path to system transaction CSV")
 	bankPath := flag.String("bank", "", "Path to bank statement CSV")
+	windowDays := flag.Int("window", 1, "Time window in days (optional, default 1)")
 	flag.Parse()
 
 	if *sysPath == "" || *bankPath == "" {
@@ -26,9 +27,8 @@ func main() {
 		log.Fatalf("Bank parse error: %v", err)
 	}
 
-	summary := Reconcile(sysTrxs, bankRecs)
-
-	fmt.Printf("--- AMARTHA RECONCILIATION REPORT ---\n")
+	summary := Reconcile(sysTrxs, bankRecs, *windowDays)
+	fmt.Printf("\n--- AMARTHA RECONCILIATION REPORT ---\n")
 	fmt.Printf("Matched : %d\n", summary.MatchedCount)
 	fmt.Printf("Unmatched : %d\n", summary.UnmatchedCount)
 	fmt.Printf("Discrepancy Total: Rp.%d,-\n", summary.TotalDiscrepancy)
